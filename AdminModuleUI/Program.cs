4cddc4c186a0ffc8b450292e2019b8abd85534d1
jubair hridoy <jubair_29@outlook.com>
2021-06-02 13:08:20 +0600

    02/06/2021

--- AdminModuleUI/Program.cs
+++ AdminModuleUI/Program.cs
@@ -16,7 +16,7 @@ namespace AdminModuleUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new adOpeningBalanceSupplier());
+            Application.Run(new adOpeningBalanceSupplierAll());
         }
     }
 }
---
+++ AdminModuleUI/adOpeningBalanceSupplierAll.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace AdminModuleUI
+{
+    partial class adOpeningBalanceSupplierAll
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OpeningBalance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SupplierName = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.OpeningDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(46)))), ((int)(((byte)(107)))));
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.OpeningBalance,
+            this.SupplierName,
+            this.OpeningDate});
+            this.dataGridView.Location = new System.Drawing.Point(-1, -1);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(503, 421);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(81)))), ((int)(((byte)(250)))));
+            this.btnSave.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSave.Location = new System.Drawing.Point(427, 426);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 42);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // Id
+            // 
+            this.Id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Visible = false;
+            // 
+            // OpeningBalance
+            // 
+            this.OpeningBalance.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.OpeningBalance.DataPropertyName = "OpeningBalance";
+            this.OpeningBalance.HeaderText = "Opening Balance";
+            this.OpeningBalance.Name = "OpeningBalance";
+            this.OpeningBalance.ReadOnly = true;
+            // 
+            // SupplierName
+            // 
+            this.SupplierName.DataPropertyName = "SupplierName";
+            this.SupplierName.HeaderText = "Supplier Name";
+            this.SupplierName.Name = "SupplierName";
+            this.SupplierName.ReadOnly = true;
+            this.SupplierName.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.SupplierName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.SupplierName.Width = 181;
+            // 
+            // OpeningDate
+            // 
+            this.OpeningDate.DataPropertyName = "OpeningDate";
+            this.OpeningDate.HeaderText = "Opening Date";
+            this.OpeningDate.Name = "OpeningDate";
+            this.OpeningDate.ReadOnly = true;
+            // 
+            // adOpeningBalanceSupplierAll
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 476);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "adOpeningBalanceSupplierAll";
+            this.Text = "adOpeningBalanceSupplierAll";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OpeningBalance;
+        private System.Windows.Forms.DataGridViewComboBoxColumn SupplierName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OpeningDate;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
